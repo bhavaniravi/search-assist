77ed4d45929ce2437b9b6ddb5e73c9da12dca120
ChristillaP <43614430+ChristillaP@users.noreply.github.com>
2020-03-21 00:19:09 +0530

    opening in new window

--- app/search.py
+++ app/search.py
@@ -8,10 +8,13 @@
 import webbrowser
 from googlesearch import search
 import sys
-
+chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+chrome_path_NW = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --new-window"
+controller = webbrowser.get(chrome_path)
+controllerNW = webbrowser.get(chrome_path_NW)
 def open_browser(urls):
     for url in urls:
-        webbrowser.open_new_tab(url)
+        controller.open(url, new=1)
 
 def search_my_term(term, count):
     return [url for url in search(term, stop=count)]
@@ -23,4 +26,5 @@ count = int(sys.argv[2])
 print (term, count)
 
 urls = search_my_term(term, count)
-open_browser(urls)
+controllerNW.open(urls[0],new=0)
+open_browser(urls[1:])
