df02cb57bb7117ef866d9228d2d96185102299bf
Olagammal <61909533+Olagammal@users.noreply.github.com>
2020-03-21 13:08:44 +0530

    Update search.py

--- app/search.py
+++ app/search.py
@@ -15,12 +15,32 @@ def open_browser(urls):
 
 def search_my_term(term, count):
     return [url for url in search(term, stop=count)]
+def search_rem(term,s,count):
+    return [url for url in search(term,start=s,stop=count)]
 
+def ignore():
+    with open("/home/olagammal/Desktop/search-assist-master/ignore.txt") as f:
+        return f.read().splitlines()
 
+url_list=[]
 term = sys.argv[1]
 count = int(sys.argv[2])
 
 print (term, count)
 
+ignore_list=ignore()
+final=0
 urls = search_my_term(term, count)
+for url in urls:
+    if url not in ignore_list:
+        url_list.append(url)
+        final+=1
+        
+        
+open_browser(url_list)
+
+
+urls=[]
+if(final!=count):
+    urls = search_rem(term,final+2,count-final)
 open_browser(urls)
